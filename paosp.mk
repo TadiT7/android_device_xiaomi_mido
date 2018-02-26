48ffc5ba14020b1378a18ac029ebc353df72e4f2
edi194 <misiakadrian@gmail.com>
2018-02-26 16:37:57 +0100

    Update paosp.mk

--- paosp.mk
+++ paosp.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 XenonHD
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,11 @@
 
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
-# Inherit some common LineageOS stuff.
+# Bootanimation stuff.
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_COPY_FILES += vendor/paosp/prebuilt/common/media/PAOSP-1080.zip:system/media/bootanimation.zip
 $(call inherit-product, vendor/paosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := paosp_mido
@@ -25,5 +29,5 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V9.0.5.0.NCFMIEI:user/release-keys" \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.0.5.0.NCFMIEI release-keys"
+    BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V9.0.3.0.NCFMIEI:user/release-keys" \
+    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.0.3.0.NCFMIEI release-keys"
